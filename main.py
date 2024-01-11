189ed64267615bb9e7476eabb93dc4f4783a091a
tusharchaudharryy <150551769+tusharchaudharryy@users.noreply.github.com>
2024-01-12 02:56:22 +0530

    completed main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from sorting_algorithms import BubbleSort, MergeSort, InsertionSort, QuickSort
 import sys
 import random
 
-# when this file is itself is ran, not by importing. 
+# when this file is itself is ran, not by importing.  
 if __name__ == '__main__':
 	ms_speed = 180  #interval in millisec by which animation will run.
 	li = []
